7acef1fe4d660650fde1421235827465b0b9e9d3
JGSims <josi3006@hotmail.com>
2020-03-13 13:19:26 -0400

    Created server file and routes directory

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", { useNewUrlParser: true });
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, () => {
+    console.log(`All systems go. Port: ${PORT}!`);
+  });
+
+
+
+
+
+// USE BELOW for models
+
+// db.Library.create({ name: "Campus Library" })
+//   .then(dbLibrary => {
+//     console.log(dbLibrary);
+//   })
+//   .catch(({message}) => {
+//     console.log(message);
+//   });
+
+// app.post("/submit", ({body}, res) => {
+//   db.Book.create(body)
+//     .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+// //
+// app.get("/books", (req, res) => {
+//   db.Book.find({})
+//     .then(dbBook => {
+//       res.json(dbBook);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/library", (req, res) => {
+//   db.Library.find({})
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/populated", (req, res) => {
+//   db.Library.find({})
+//     .populate("books")
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
